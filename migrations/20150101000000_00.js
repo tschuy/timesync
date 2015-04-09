3fbeab142103c5e6c34e0ba6527a05ea9567c188
Evan Tschuy <tschuy@osuosl.org>
2015-04-09 15:40:59 -0700

    Make slug columns unique

--- migrations/20150101000000_00.js
+++ migrations/20150101000000_00.js
@@ -4,13 +4,13 @@ exports.up = function(knex, Promise) {
    return knex.schema.createTable('projects', function (table) {
     table.increments('id').primary();
     table.string('name');
-    table.string('slug');
+    table.string('slug').unique();
     table.string('uri');
     table.integer('owner').references('id').inTable('users');
   }).createTable('time_entries', function (table) {
     table.increments('id').primary();
     table.integer('duration');
-    table.integer('users').references('id').inTable('users');
+    table.integer('user').references('id').inTable('users');
     table.integer('project').references('id').inTable('projects');
     table.integer('activity').references('id').inTable('activity');
     table.string('notes');
@@ -20,10 +20,10 @@ exports.up = function(knex, Promise) {
   }).createTable('activities', function (table) {
     table.increments('id').primary();
     table.string('name');
-    table.string('slug');
+    table.string('slug').unique();
   }).createTable('users', function (table) {
     table.increments('id').primary();
-    table.string('username');
+    table.string('username').unique();
   });
 };
 
