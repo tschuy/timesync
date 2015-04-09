2fcfbaeb21c1734131a15c0170ca9b265083921b
Evan Tschuy <tschuy@osuosl.org>
2015-04-09 13:47:42 -0700

    Begin activities/add

--- app.js
+++ app.js
@@ -12,9 +12,7 @@ app.use(knex({
 )
 
 app.use(_.get('/users/:userid', function *(userid) {
-    this.body = {
-      profile: yield this.knex('users').where('id', userid)
-    };
+    this.body = yield this.knex('users').where('id', userid);
 }));
 
 app.use(_.get('/users', function *() {
@@ -22,8 +20,28 @@ app.use(_.get('/users', function *() {
 }));
 
 app.use(_.post('/users/add', function *() {
+    var id = (yield this.knex('users').insert({username: this.request.query['username']}))[0];
     this.body = {
-      profile: yield this.knex('users').insert({username: this.request.query['username']})
+      profile: yield this.knex('users').where('id', id)
+    };
+}));
+
+app.use(_.post('/activities/add', function *() {
+    var slug = this.request.query['slug'];
+    if(! slug) {
+        slug = this.request.query['name'].toLowerCase()
+            .replace(/ /g,'-')
+            .replace(/[^\w-]+/g,'')
+        ;
+    }
+    var id = (yield this.knex('activities').insert(
+        {name: this.request.query['name'],
+        slug: slug}))[0];
+
+    console.log(id);
+
+    this.body = {
+      profile: yield this.knex('activities').where('id', id)
     };
 }));
 
--- migrations/20150101000000_00.js
+++ migrations/20150101000000_00.js
@@ -2,12 +2,13 @@
 
 exports.up = function(knex, Promise) {
    return knex.schema.createTable('projects', function (table) {
-    table.integer('id').primary();
+    table.increments('id').primary();
     table.string('name');
     table.string('slug');
     table.string('uri');
     table.integer('owner').references('id').inTable('users');
   }).createTable('time_entries', function (table) {
+    table.increments('id').primary();
     table.integer('duration');
     table.integer('users').references('id').inTable('users');
     table.integer('project').references('id').inTable('projects');
@@ -17,11 +18,11 @@ exports.up = function(knex, Promise) {
     table.timestamp('date_worked');
     table.timestamps();
   }).createTable('activities', function (table) {
-    table.integer('id').primary();
+    table.increments('id').primary();
     table.string('name');
     table.string('slug');
   }).createTable('users', function (table) {
-    table.increments('id');
+    table.increments('id').primary();
     table.string('username');
   });
 };
