72b5c92475e08e7dafaf3bf600fd3ca6a02e8921
Evan Tschuy <tschuy@osuosl.org>
2015-04-09 12:13:53 -0700

    Update migrations with time entries, activities

--- migrations/20150101000000_00.js
+++ migrations/20150101000000_00.js
@@ -3,13 +3,24 @@
 exports.up = function(knex, Promise) {
    return knex.schema.createTable('projects', function (table) {
     table.integer('id').primary();
-    table.timestamp('created');
-    table.timestamp('modified');
     table.string('name');
     table.string('slug');
     table.string('uri');
     table.string('owner');
-  };
+  }).createTable('time_entries', function (table) {
+    table.integer('duration');
+    table.string('user');
+    table.integer('project').references('id').inTable('projects');
+    table.integer('activity').references('id').inTable('activity');
+    table.string('notes');
+    table.string('issue_uri');
+    table.timestamp('date_worked');
+    table.timestamps();
+  }).createTable('activities', function (table) {
+    table.integer('id').primary();
+    table.string('name');
+    table.string('slug');
+  });
 };
 
 exports.down = function(knex, Promise) {
