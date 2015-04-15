80332e6685bb190901bb3c0ac39f864b839b2ecd
Ian Kronquist <iankronquist@teleport.com>
2015-04-14 18:49:37 -0700

    Rewrite paths in load_fixtures helper script

--- scripts/load_fixtures.js
+++ scripts/load_fixtures.js
@@ -1,8 +1,8 @@
 var sqlFixtures = require('sql-fixtures');
-var knexfile = require('./knexfile');
+var knexfile = require('../knexfile');
 var knex = require('knex')(knexfile.development);;
 
-var test_data = require('./tests/fixtures/test_data');
+var test_data = require('../tests/fixtures/test_data');
 
 sqlFixtures.create(knexfile.development, test_data).then(function () {
   process.exit(0);
